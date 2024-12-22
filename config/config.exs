93eddd004df8e6f5b51c0b6edf9539f89b533c83
Owen <owenrees82@gmail.com>
2024-12-22 20:37:35 +0100

    config and use of env var changed

--- config/config.exs
+++ config/config.exs
@@ -3,18 +3,19 @@ import Config
 config :plantid_discord_bot,
   max_results: 5,
   score_threshold: 0.3,
-  plantnet_api_key: System.get_env("PLANTNET_API_KEY"),
-  plantnet_api_base_url: "https://my-api.plantnet.org/v2",
-  port: 4321,
   guild_request_limit_per_day: 20,
   image_path: "priv/static",
-  fileserver_url: "http://localhost:4321",
+  plantnet_api_base_url: "https://my-api.plantnet.org/v2",
   source: "https://github.com/TheRealOwenRees/plantid-discord-bot",
   invite:
     "https://discord.com/api/oauth2/authorize?client_id=948227126094598204&permissions=19520&scope=bot"
 
 config :nostrum,
-  token: System.get_env("PLANTID_DISCORD_BOT_TOKEN"),
   ffmpeg: nil
+
+config :plantid_discord_bot, PlantIdDiscordBot.Scheduler,
+  jobs: [
+    {"@daily", {PlantIdDiscordBot.RateLimiter, :reset_counters, []}}
+  ]
 
 import_config "#{config_env()}.exs"
