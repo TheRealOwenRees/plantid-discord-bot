c06ace77fcf4713e61c5f3477ff78925db03c416
Owen <owenrees82@gmail.com>
2024-12-22 11:38:41 +0100

    rename env vars

--- config/prod.exs
+++ config/prod.exs
@@ -9,6 +9,6 @@ config :logger,
   backends: [{PlantIdDiscordBot.DiscordLogger, :discord_logger}]
 
 config :logger, :discord_logger,
-  webhook_url: System.get_env("LOGS_DISCORD_WEBHOOK_URL"),
+  webhook_url: System.get_env("PLANTID_LOGS_DISCORD_WEBHOOK_URL"),
   level: :error,
-  bot_token: System.get_env("DISCORD_TOKEN")
+  bot_token: System.get_env("PLANTID_DISCORD_BOT_TOKEN")
