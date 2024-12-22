593704cb150145df9ad32b0b04039e906717c713
Owen <owenrees82@gmail.com>
2024-12-22 20:58:01 +0100

    port update in prod

--- lib/application.ex
+++ lib/application.ex
@@ -1,15 +1,16 @@
 defmodule PlantIdDiscordBot.Application do
   use Application
 
-  @port Application.compile_env(:plantid_discord_bot, :port)
-
   @impl true
   def start(_type, _args) do
     children = [
       PlantIdDiscordBot.Consumer,
       PlantIdDiscordBot.RateLimiter,
       PlantIdDiscordBot.Scheduler,
-      {Plug.Cowboy, scheme: :http, plug: PlantIdDiscordBot.FileServer, options: [port: @port]}
+      {Plug.Cowboy,
+       scheme: :http,
+       plug: PlantIdDiscordBot.FileServer,
+       options: [port: Application.get_env(:plantid_discord_bot, :port)]}
     ]
 
     opts = [strategy: :one_for_one, name: PlantIdDiscordBot.Supervisor]
