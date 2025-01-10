a0ea25503850a0640e1cffa5ad383c31ea887adf
Owen <owenrees82@gmail.com>
2025-01-10 20:06:34 +0100

    update release info

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlantidDiscordBot.MixProject do
   def project do
     [
       app: :plantid_discord_bot,
-      version: "0.1.2",
+      version: "0.2.0-alpha",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
