84bbafb441c7e1f74e4bae8582629037d3243fd9
Owen <owenrees82@gmail.com>
2025-01-10 11:48:15 +0100

    updated tests

--- test/metrics_test.exs
+++ test/metrics_test.exs
@@ -13,19 +13,35 @@ defmodule PlantIdDiscordBot.MetricsTest do
 
     assert length(keys) == 2
 
-    assert %PlantIdDiscordBot.Metrics{
-             guild_id: 123,
-             guild_name: "Test Guild 1",
-             total_requests: 1
-           } ==
-             PlantIdDiscordBot.Metrics.get(123)
+    %PlantIdDiscordBot.Metrics{
+      guild_id: guild_id1,
+      guild_name: guild_name1,
+      first_request_at: first_request_at1,
+      last_request_at: last_request_at1,
+      total_requests: total_requests1
+    } =
+      PlantIdDiscordBot.Metrics.get(123)
 
-    assert %PlantIdDiscordBot.Metrics{
-             guild_id: 321,
-             guild_name: "Test Guild 2",
-             total_requests: 1
-           } ==
-             PlantIdDiscordBot.Metrics.get(321)
+    assert guild_id1 == 123
+    assert guild_name1 == "Test Guild 1"
+    assert %DateTime{} = first_request_at1
+    assert %DateTime{} = last_request_at1
+    assert total_requests1 == 1
+
+    %PlantIdDiscordBot.Metrics{
+      guild_id: guild_id2,
+      guild_name: guild_name2,
+      first_request_at: first_request_at2,
+      last_request_at: last_request_at2,
+      total_requests: total_requests2
+    } =
+      PlantIdDiscordBot.Metrics.get(321)
+
+    assert guild_id2 == 321
+    assert guild_name2 == "Test Guild 2"
+    assert %DateTime{} = first_request_at2
+    assert %DateTime{} = last_request_at2
+    assert total_requests2 == 1
   end
 
   test "update requests for a guild" do
@@ -33,11 +49,19 @@ defmodule PlantIdDiscordBot.MetricsTest do
     PlantIdDiscordBot.Metrics.put(123, "Test Guild 1")
     PlantIdDiscordBot.Metrics.put(123, "Test Guild 1")
 
-    assert %PlantIdDiscordBot.Metrics{
-             guild_id: 123,
-             guild_name: "Test Guild 1",
-             total_requests: 2
-           } ==
-             PlantIdDiscordBot.Metrics.get(123)
+    %PlantIdDiscordBot.Metrics{
+      guild_id: guild_id,
+      guild_name: guild_name,
+      first_request_at: first_request_at,
+      last_request_at: last_request_at,
+      total_requests: total_requests
+    } =
+      PlantIdDiscordBot.Metrics.get(123)
+
+    assert guild_id == 123
+    assert guild_name == "Test Guild 1"
+    assert %DateTime{} = first_request_at
+    assert %DateTime{} = last_request_at
+    assert total_requests == 2
   end
 end
