18b1c027336005f5f510ec09a848128bac7427b3
Owen <owenrees82@gmail.com>
2025-01-15 23:10:05 +0100

    upgrade logger webhook backend

--- test/error_handling_test.exs
+++ test/error_handling_test.exs
@@ -13,6 +13,8 @@ defmodule PlantIdDiscordBot.ErrorHandlingTest do
       assert_received {:create_message, 123456, content: "An error has occured. Please try again later."}
     end)
 
+    IO.inspect(log)
+
       assert log =~ "guild_id=#{message.guild_id}"
       assert log =~ "guild_name=#{@guild.get_guild_name!(message.guild_id)}"
   end
