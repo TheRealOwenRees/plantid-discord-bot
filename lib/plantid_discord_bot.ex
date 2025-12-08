8f2534ec7a8eb3e8806a07c163e4276e36473e5d
Owen Rees <owenrees.dev@gmail.com>
2025-12-08 10:41:14 +0100

    multiclause function event handler

--- lib/plantid_discord_bot.ex
+++ lib/plantid_discord_bot.ex
@@ -18,7 +18,6 @@ defmodule PlantIdDiscordBot.Consumer do
       Api.create_guild_application_command(guild_id, cmd)
     end)
 
-    # Only register global commands in production
     if Mix.env() == :prod do
       Api.create_global_application_command(@global_application_commands)
     end
@@ -31,17 +30,7 @@ defmodule PlantIdDiscordBot.Consumer do
   end
 
   def handle_event({:INTERACTION_CREATE, %{data: %{name: command}} = interaction, _ws_state}) do
-    case command do
-      "source" -> Cog.Info.source(interaction)
-      "invite" -> Cog.Info.invite(interaction)
-      "help" -> Cog.Info.help(interaction)
-      "info" -> Cog.Info.info(interaction)
-      "stats" -> Cog.Info.stats(interaction)
-      "status" -> Cog.Info.status(interaction)
-      "servers" -> Cog.Info.servers(interaction)
-      "diseases" -> Cog.Diseases.diseases(interaction)
-      "projects" -> Cog.Projects.projects(interaction)
-    end
+    handle_interaction(command, interaction)
   end
 
   def handle_event({:MESSAGE_CREATE, %{attachments: attachments} = message, _ws_state}) do
@@ -50,4 +39,14 @@ defmodule PlantIdDiscordBot.Consumer do
       Cog.PlantNetMessage.id(message)
     end
   end
+
+  defp handle_interaction("source", interaction), do: Cog.Info.source(interaction)
+  defp handle_interaction("invite", interaction), do: Cog.Info.invite(interaction)
+  defp handle_interaction("help", interaction), do: Cog.Info.help(interaction)
+  defp handle_interaction("info", interaction), do: Cog.Info.info(interaction)
+  defp handle_interaction("stats", interaction), do: Cog.Info.stats(interaction)
+  defp handle_interaction("status", interaction), do: Cog.Info.source(interaction)
+  defp handle_interaction("servers", interaction), do: Cog.Info.status(interaction)
+  defp handle_interaction("diseases", interaction), do: Cog.Diseases.diseases(interaction)
+  defp handle_interaction("projects", interaction), do: Cog.Projects.projects(interaction)
 end
